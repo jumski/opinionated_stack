13df4449ca93979b3c6214707fbebd70248d76b9
Akra Polska <office@akra.net>
2012-06-26 13:39:18 +0200

    databases works like a charm

--- cookbooks/akra_essentials/attributes/default.rb
+++ cookbooks/akra_essentials/attributes/default.rb
@@ -1,4 +1,16 @@
 
+applications = %w(akra_polska epics)
+
+default['applications'] = applications.map do |name|
+  {
+    :username    => name,
+    :password    => "#{name}_deploy_password",
+    :db_username => name,
+    :db_name     => name,
+    :db_password => "#{name}_mysql_password"
+  }
+end
+
 mysql["server_root_password"]   = "akrapolskalubimysql"
 mysql["server_repl_password"]   = "akrapolskalubimysql"
 mysql["server_debian_password"] = "akrapolskalubimysql"
--- cookbooks/akra_essentials/recipes/default.rb
+++ cookbooks/akra_essentials/recipes/default.rb
@@ -1,17 +1,9 @@
 
 include_recipe "mysql::server"
+include_recipe "database"
 include_recipe "supervisor"
 
 package "libshadow-ruby1.8"
-user "deploy" do
-  comment "Random User"
-  uid 2001
-  gid "deploy"
-  home "/home/deploy"
-  shell "/bin/bash"
-  password "$1$WOnSGbsW$jGaquFTfyPRccRQ9w1p3/1"
-  supports({:manage_home => true})
-end
 
 group "deploy" do
   gid 2001
@@ -20,5 +12,33 @@ end
 directory "/etc/supervisor.d" do
   group "deploy"
   mode "770"
+end
+
+connection_info = {:host => "localhost", :username => 'root', :password => node['mysql']['server_root_password']}
+
+node['applications'].each do |cfg|
+
+  user cfg[:username] do
+    gid "deploy"
+    home "/home/#{cfg[:username]}"
+    shell "/bin/bash"
+    password %x[ openssl passwd -1 #{cfg[:password]} ].chomp!
+    supports({:manage_home => true})
+  end
+
+  mysql_database cfg[:db_name] do
+    connection connection_info
+    action :create
+  end
+
+  mysql_database_user cfg[:db_username] do
+    connection connection_info
+    password cfg[:db_password]
+    database_name cfg[:db_name]
+    host '%'
+    privileges [:select, :update, :insert, :create, :drop]
+    action :grant
+  end
+
 end
 
