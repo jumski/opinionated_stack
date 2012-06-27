4930a3ff01b798806c5c3212d99a440461f173ec
Akra Polska <office@akra.net>
2012-06-27 12:11:56 +0200

    fixed?

--- cookbooks/akra_essentials/attributes/default.rb
+++ cookbooks/akra_essentials/attributes/default.rb
@@ -1,45 +1,49 @@
 
-default['akra_essentials'].tap do |akra|
+default[:akra_essentials].tap do |akra|
 
   # packages
-  akra['packages'] = %w{
+  akra[:packages] = %w{
     libshadow-ruby1.8
     imagemagick
     libmagickcore-dev
   }
 
-  # akra[:rvm_wrapper_prefix] = 'sys'
-  # hax = "#{default['rvm']['root_path']}/#{akra[:rvm_wrapper_prefix]}_"
-  # akra['rvm_wrapper_path_prefix'] = hax
-  # akra['unicorn_bin_path'] = "#{akra[:rvm_wrapper_path_prefix]}unicorn"
-  akra['rvm_wrapper_path_prefix'] = 'hax'
-  akra['unicorn_bin_path'] = 'hax'
+  akra[:rvm_wrapper_prefix] = 'sys'
 
-  # create basic app values based on app name
-  %w(akra_polska epics).each do |name|
-    akra['apps'][name]['name']        = name
-    akra['apps'][name]['home_dir']    = "/home/#{name}"
-    akra['apps'][name]['username']    = name
-    akra['apps'][name]['password']    = "#{name}_deploy_password"
-    akra['apps'][name]['db_username'] = name
-    akra['apps'][name]['db_name']     = name
-    akra['apps'][name]['db_password'] = "#{name}_mysql_password"
-    akra['apps'][name]['domains']     = []
-    akra['apps'][name]['unicorn_config_path'] = "#{akra['apps'][name]['home_dir']}/shared/config/unicorn.rb"
-    akra['apps'][name]['unicorn_socket_path'] = "#{akra['apps'][name]['home_dir']}/current/tmp/unicorn.sock"
-    akra['apps'][name]['unicorn_worker_processes'] = 1
-  end
 
-  raise akra['apps']['epics']['name'].inspect
-
-  # overrides
-  akra['apps'].tap do |apps|
-    apps['akra_polska']['domains'] = %w(akra.net)
-    apps['epics']['domains']       = %w(epics.pl)
-    apps['epics']['unicorn_worker_processes'] = 6
+  akra['apps'] = %w(akra_polska epics).map do |name|
+    {
+      :name => name,
+      :home_dir => home_dir = "/home/#{name}",
+      :username => name,
+      :password => "#{name}_deploy_password",
+      :db_username => name,
+      :db_name => name,
+      :db_password => "#{name}_mysql_password",
+      :domains => [],
+      :unicorn_config_path => "#{home_dir}/shared/config/unicorn.rb",
+      :unicorn_socket_path => "#{home_dir}/current/tmp/unicorn.sock",
+      :unicorn_worker_processes => 1,
+    }
   end
 
 end
+
+# akra['rvm_wrapper_path_prefix'] = hax
+# akra['unicorn_bin_path'] = "#{akra[:rvm_wrapper_path_prefix]}unicorn"
+# akra['rvm_wrapper_path_prefix'] = 'hax'
+# akra['unicorn_bin_path'] = 'hax'
+
+#   # create basic app values based on app name
+
+# raise akra['apps']['epics']['name'].inspect
+#
+# # overrides
+# akra['apps'].tap do |apps|
+#   apps['akra_polska']['domains'] = %w(akra.net)
+#   apps['epics']['domains']       = %w(epics.pl)
+#   apps['epics']['unicorn_worker_processes'] = 6
+# end
 
 # mysql passwords
 mysql["server_root_password"]   = "akrapolskalubimysql"
--- cookbooks/akra_essentials/recipes/default.rb
+++ cookbooks/akra_essentials/recipes/default.rb
@@ -10,6 +10,8 @@ include_recipe "rvm::system"
 akra = node[:akra_essentials]
 
 akra[:packages].each do |name|
+  puts '----------------'
+  puts name.inspect
   package name
 end
 
@@ -22,9 +24,9 @@ directory "/etc/supervisor.d" do
   mode "770"
 end
 
-rvm_wrapper akra[:bundle_wrapper_prefix] do
+rvm_wrapper akra[:rvm_wrapper_prefix] do
   ruby_string "default@global"
-  binary "bundle"
+  binary "unicorn_rails"
 end
 
 ###########################################
@@ -35,76 +37,70 @@ connection_info = {
   :username => 'root',
   :password => node[:mysql][:server_root_password]
 }
-akra['apps'].keys.each do |name|
-
-  app = akra['apps'][name]
-
-  raise name.inspect
-
-  # unix user
-  user app[:username] do
-    gid "deploy"
-    home app[:home_dir]
-    shell "/bin/bash"
-    password %x[ openssl passwd -1 #{app[:password]} ].chomp!
-    supports({:manage_home => true})
-  end
-
-  # mysql db with user
-  mysql_database app[:db_name] do
-    connection connection_info
-    action :create
-  end
-  mysql_database_user app[:db_username] do
-    connection connection_info
-    password app[:db_password]
-    database_name app[:db_name]
-    host '%'
-    privileges [:select, :update, :insert, :create, :drop]
-    action :grant
-  end
-
-  # nginx site config
-  template "/etc/nginx/sites-available/#{name}" do
-    source "rails_site.erb"
-    mode 700
-    owner "root"
-    group "root"
-    variables(
-      :domains     => app[:domains].map{|d| [d, "www.#{d}"]}.flatten,
-      :name        => name,
-      :socket_path => app[:unicorn_socket_path],
-      :root        => "#{app[:home_dir]}/current"
-    )
-  end
-
-  # nginx site config
-  template app[:unicorn_config_path] do
-    source "unicorn_rails_config.erb"
-    mode 700
-    owner app[:username]
-    group 'deploy'
-    variables(
-      :username         => app[:username],
-      :group            => app[:group],
-      :worker_processes => app[:unicorn_worker_processes],
-      :root             => "#{app[:home_dir]}/current",
-      :timeout          => 30,
-      :preload_app      => true
-    )
-  end
-
-  supervisor_service "#{name}_unicorn" do
-    action :enable
-    command "#{a}"
-
-    autostart true
-    autorestart false
-    user "deploy"
-    stdout_logfile "#{app[:home_dir]}/current/log/unicorn-out.log"
-    stderr_logfile "#{app[:home_dir]}/current/log/unicorn-err.log"
-    directory "#{app[:home_dir]}/current"
-  end
-
-end
+#   # unix user
+#   user app[:username] do
+#     gid "deploy"
+#     home app[:home_dir]
+#     shell "/bin/bash"
+#     password %x[ openssl passwd -1 #{app[:password]} ].chomp!
+#     supports({:manage_home => true})
+#   end
+#
+#   # mysql db with user
+#   mysql_database app[:db_name] do
+#     connection connection_info
+#     action :create
+#   end
+#   mysql_database_user app[:db_username] do
+#     connection connection_info
+#     password app[:db_password]
+#     database_name app[:db_name]
+#     host '%'
+#     privileges [:select, :update, :insert, :create, :drop]
+#     action :grant
+#   end
+#
+#   # nginx site config
+#   template "/etc/nginx/sites-available/#{name}" do
+#     source "rails_site.erb"
+#     mode 700
+#     owner "root"
+#     group "root"
+#     variables(
+#       :domains     => app[:domains].map{|d| [d, "www.#{d}"]}.flatten,
+#       :name        => name,
+#       :socket_path => app[:unicorn_socket_path],
+#       :root        => "#{app[:home_dir]}/current"
+#     )
+#   end
+#
+#   # nginx site config
+#   template app[:unicorn_config_path] do
+#     source "unicorn_rails_config.erb"
+#     mode 700
+#     owner app[:username]
+#     group 'deploy'
+#     variables(
+#       :username         => app[:username],
+#       :group            => app[:group],
+#       :worker_processes => app[:unicorn_worker_processes],
+#       :root             => "#{app[:home_dir]}/current",
+#       :timeout          => 30,
+#       :preload_app      => true
+#     )
+#   end
+#
+#   supervisor_service "#{name}_unicorn" do
+#     action :enable
+#     command "#{a}"
+#
+#     autostart true
+#     autorestart false
+#     user "deploy"
+#     stdout_logfile "#{app[:home_dir]}/current/log/unicorn-out.log"
+#     stderr_logfile "#{app[:home_dir]}/current/log/unicorn-err.log"
+#     directory "#{app[:home_dir]}/current"
+#   end
+#
+# end
 
