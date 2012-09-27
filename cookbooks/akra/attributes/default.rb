5a067ffa11b48d3ccba41972c8ad4132529a0970
Akra Polska <office@akra.net>
2012-09-27 15:50:42 +0200

    some debs

--- cookbooks/akra/attributes/default.rb
+++ cookbooks/akra/attributes/default.rb
@@ -6,6 +6,8 @@ default[:akra][:packages] = %w{
   imagemagick
   libmagickcore-dev
   vim-nox
+  htop
+  ack-grep
 }
 default[:akra][:rvm_wrapper_prefix] = 'sys'
 default[:akra][:bundler_bin_path]   = "/usr/local/rvm/bin/sys_bundle"
