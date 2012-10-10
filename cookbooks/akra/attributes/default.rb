84baecba54b01064847ec67220a01ac2e109e2ef
Wojtek Majewski <jumski@gmail.com>
2012-10-10 22:50:45 +0200

    disable default mysql passwords for overriding in json

--- cookbooks/akra/attributes/default.rb
+++ cookbooks/akra/attributes/default.rb
@@ -18,11 +18,6 @@ default[:akra][:packages] = %w{
 default[:akra][:rvm_wrapper_prefix] = 'sys'
 default[:akra][:bundler_bin_path]   = "/usr/local/rvm/bin/sys_bundle"
 
-# mysql passwords
-mysql["server_root_password"]   = "akrapolskalubimysql"
-mysql["server_repl_password"]   = "akrapolskalubimysql"
-mysql["server_debian_password"] = "akrapolskalubimysql"
-
 # nginx defaults
 nginx[:user] = "deploy"
 
